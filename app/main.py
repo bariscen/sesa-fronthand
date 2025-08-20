9083dd3afc531d7f81aa5d55effbcce4932c664c
Baris Cen <baris.cen@hotmail.com>
2025-08-20 10:09:05 +0100

    s

--- app/main.py
+++ app/main.py
@@ -4,11 +4,7 @@ import pandas as pd
 import os
 from pathlib import Path
 
-import sys, pathlib
-print("CWD=", pathlib.Path().resolve())
-print("sys.path[:3]=", sys.path[:3]
 
-st.set_page_config(initial_sidebar_state="collapsed")
 
 st.markdown(
     """
